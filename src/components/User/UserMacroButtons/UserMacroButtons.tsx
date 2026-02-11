1822c3d4aa6497a1deea229a190522aaf6873855
MARIO\mario <mariobes01@gmail.com>
2026-02-11 15:13:24 +0100

    Add intakeService, intakeStore and modify UserMacroChart

--- src/components/User/UserMacroButtons/UserMacroButtons.tsx
+++ src/components/User/UserMacroButtons/UserMacroButtons.tsx
@@ -1,27 +1,38 @@
+import { useState } from "react";
 import { Box, Button } from "@mui/material";
 import IconAdd from '@mui/icons-material/AddCircleOutline';
 import styles from "./UserMacroButtons.module.css";
+import CreateFoodDialog from "./Dialogs/CreateFood/CreateFoodDialog";
 
 export default function UserMacroButtons() {
+	const [openFood, setOpenFood] = useState(false);
+	const [openMeal, setOpenMeal] = useState(false);
+	const [openIntake, setOpenIntake] = useState(false);
+	const [openWater, setOpenWater] = useState(false);
 
   return (
 		<Box className={styles['container-buttons']} mt={3}>
-			<Button variant="contained" className={styles['container-buttons-btn']} /*onClick={handleCreateFood}*/
+			<Button variant="contained" className={styles['container-buttons-btn']} onClick={() => setOpenFood(true)}
 				endIcon={<IconAdd style={{ fontSize: 22, marginTop: 3 }} />}>
 				Crear alimento
 			</Button>
-			<Button variant="contained" className={styles['container-buttons-btn']} /*onClick={handleCreateMeal}*/
+			<Button variant="contained" className={styles['container-buttons-btn']} onClick={() => setOpenMeal(true)}
 				endIcon={<IconAdd style={{ fontSize: 22, marginTop: 3 }} />}>
 				Crear comida
 			</Button>
-			<Button variant="contained" className={styles['container-buttons-btn']} /*onClick={handleAddRecord}*/
+			<Button variant="contained" className={styles['container-buttons-btn']} onClick={() => setOpenIntake(true)}
 				endIcon={<IconAdd style={{ fontSize: 22, marginTop: 3 }} />}>
 				Añadir registro
 			</Button>
-			<Button variant="contained" className={styles['container-buttons-btn']} /*onClick={handlePutWater}*/
+			<Button variant="contained" className={styles['container-buttons-btn']} onClick={() => setOpenWater(true)}
 				endIcon={<IconAdd style={{ fontSize: 22, marginTop: 3 }} />}>
 				Introducir agua 
 			</Button>
+
+			<CreateFoodDialog open={openFood} onClose={() => setOpenFood(false)} />
+			{/* <CreateMealDialog open={openMeal} onClose={() => setOpenMeal(false)} />
+		  <CreateIntakeDialog open={openIntake} onClose={() => setOpenIntake(false)} />
+		  <PutWaterDialog open={openWater} onClose={() => setOpenWater(false)} /> */}
 		</Box>
   );
 }
