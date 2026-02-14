7bf820992dccdefb3b04c2508ecfcbf8cecbaa37
MARIO\mario <mariobes01@gmail.com>
2026-02-14 21:00:34 +0100

    Create APK for mobile

---
+++ android/app/src/main/java/com/mario/nutricore/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mario.nutricore;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Guards } from "./guards";
 import AuthPage from "@/pages/AuthPage/AuthPage";
 import UserPrivatePage from "@/pages/UserPrivatePage/UserPrivatePage";
 
 export default function Router() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<AuthPage />} />
         <Route path="/userPrivate/:id" element={<Guards><UserPrivatePage /></Guards>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
